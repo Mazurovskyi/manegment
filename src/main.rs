302282a12dc12739d2b9c64fe50523d69583b381
Yevhenii Mazurovskyi <mazurovskiyyevgen@gmail.com>
2022-04-21 14:00:16 +0300

    add_empl_editing_with_discriminant

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::collections::HashMap;
+use std::mem::discriminant;
 
 const QUOT: char = '"';
 
@@ -18,6 +19,7 @@ enum Option<T>{
     None
 }
 
+#[derive(Debug)]
 enum Sex{
     male,
     feemale,
@@ -25,6 +27,7 @@ enum Sex{
     other
 }
 
+#[derive(Debug)]
 enum Department{
     developer,
     admin,
@@ -32,6 +35,7 @@ enum Department{
     accountant
 }
 
+#[derive(Debug)]
 struct Employee{
     name: String,
     sex: Sex,
@@ -45,11 +49,16 @@ struct Employee{
 fn main() {
     println!("\n{headline:>width$}\n", headline = "---Wellcome to sky-armor---", width=60);
     let com = format!("Enter command (to get instructions enter {0}help{0}): ", QUOT);
+
+
+    let mut employee_vect: Vec<Employee> = Vec::new();
+
+
     'menu: loop{
         let command = enter_string(&com).turn_command();
         match command{
             Command::help => {help(); continue 'menu},
-            Command::add_empl => add_empl(),
+            Command::add_empl => add_empl(&mut employee_vect),
             Command::rem_empl => rem_empl(),
             Command::rebase_empl => rebase_empl(),
             Command::show_empl_dep => show_empl_dep(),
@@ -61,11 +70,62 @@ fn main() {
 }
 
 
-fn add_empl(){
+fn add_empl(employee_vect: &mut Vec<Employee>){
     println!("      Adding new employee: ");
-    let mut sex_counter = 0; 
 
-    let X = Employee{
+    let mut val: bool = true;
+    
+    let mut new_employee = new_employee();
+
+    for worker in employee_vect{
+        if discriminant(&worker.department) == discriminant(&new_employee.department){
+            if worker.chief == true{                                
+                val = false
+            }                           
+        }
+    }
+
+    //employee_vect.push(new_employee); 
+
+    match val{
+        true => {
+            'enter_chief: loop{
+                let message = format!("Would you like to make {0} a chief of department? (Enter {1}yes{1} or {1}no{1})", new_employee.name, QUOT);
+                match enter_string(&message){
+                    Option::Some(emp_chief) => {
+                        match emp_chief.trim(){
+
+                            "yes" => {
+                                new_employee.chief = true; 
+                                employee_vect.push(new_employee); 
+                                break 'enter_chief
+                            },
+                            "no" => {
+                                employee_vect.push(new_employee); 
+                                break 'enter_chief
+                            },
+                            _=> {
+                                println!("Please, enter {0}yes{0} or {0}no{0}!", QUOT); 
+                                continue 'enter_chief
+                            }
+
+                        }
+                    },
+                    Option::None => {println!("Please, enter something"); continue 'enter_chief}
+                }
+            }
+        }
+        false => employee_vect.push(new_employee)
+    }
+    
+}
+
+
+
+
+fn new_employee()-> Employee{
+    let mut sex_counter = 0;
+    Employee{
         name: {
             'enter_name: loop{
                 match enter_string("Name: "){
@@ -124,7 +184,7 @@ fn add_empl(){
             'enter_age: loop{
                 match enter_string("Education: "){
                     Option::Some(emp_ege) => {
-                        match emp_ege.trim().parse(){                                       //Указываем неявно тип, к которому парсим???
+                        match emp_ege.trim().parse(){                                       //компилятор знает к какому типу парсить????
                             Result::Ok(emp_ege) => break 'enter_age emp_ege,
                             Result::Err(message) => {println!("Enter the quess! {}", message); continue 'enter_age}
                         }
@@ -138,10 +198,10 @@ fn add_empl(){
                 match enter_string("Department (developer / admin / engineering / accountant): "){
                     Option::Some(emp_dep) => {
                         match emp_dep.trim(){
-                            "developer" => break 'enter_department Department::developer,
-                            "admin" => break 'enter_department Department::admin,
-                            "engineering" => break 'enter_department Department::engineering,
-                            "accountant" => break 'enter_department Department::accountant,
+                            "developer" => {break 'enter_department Department::developer},
+                            "admin" => {break 'enter_department Department::admin},
+                            "engineering" => {break 'enter_department Department::engineering},
+                            "accountant" => {break 'enter_department Department::accountant},
                             _=> {
                                     println!("Enter one of the departments!"); continue 'enter_department                                                          
                             }
@@ -151,30 +211,13 @@ fn add_empl(){
                 }
             }
         },
-        chief:{
-            'enter_chief: loop{
-                match enter_string("Chief? (true or false): "){
-                    Option::Some(emp_chief) => {
-                        match emp_chief.trim(){
-                            "true"=> {
-                                //break 'enter_chief true
-                            },
-                            "false" => break 'enter_chief false,
-                            _=> {println!("You need to enter {0}true{0} or {0}false{0}", QUOT); continue 'enter_chief}
-                        }
-
-                    }
-                    Option::None => {println!("Enter the employee status"); continue 'enter_chief}
-                }
-            }
-        }
-
-        
-
-
-    };
-    
+        chief: false
+    }
 }
+
+
+
+
 
 
 
@@ -221,6 +264,12 @@ fn help(){
             el4 = "show_empl_dep", 
             el5 = "help");
 }
+
+
+
+
+
+
 
 
 
